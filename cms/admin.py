78f5dfe8213994ac9fe438342f55a4cb7f1de5f2
kaoru morito <monostown@gmail.com>
2017-10-10 01:11:35 +0900

    modify_DuelistTournament

--- cms/admin.py
+++ cms/admin.py
@@ -1,14 +1,19 @@
 from django.contrib import admin
-from cms.models import Duelist, Tournament
+from cms.models import Duelist, Tournament, DuelistTournament
 
 class DuelistAdmin(admin.ModelAdmin):
-    list_display = ('id', 'name', 'win', 'lose', 'draw',)
-    list_display_link = ('id', 'name', 'win', 'lose', 'draw',)
+    list_display = ('id', 'name',)
+    list_display_link = ('id', 'name',)
 
 class TournamentAdmin(admin.ModelAdmin):
     list_display = ('id', 'name',)
     list_display_link = ('id', 'name',)
 
+class DuelistTournamentAdmin(admin.ModelAdmin):
+    list_display = ('id','win', 'lose', 'draw',)
+    list_display_link = ('id', 'win', 'lose', 'draw',)
+
 # Register your models here.
 admin.site.register(Duelist, DuelistAdmin)
 admin.site.register(Tournament, TournamentAdmin)
+admin.site.register(DuelistTournament, DuelistTournamentAdmin)
